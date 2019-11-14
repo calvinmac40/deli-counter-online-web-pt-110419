6e11a4a83e0a581ebf894a4c5e71676e42692656
Calvin McDowell <calvinmac40@hotmail.com>
2019-11-14 02:44:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,6 +20,6 @@ def take_a_number(deli,name)
     puts "Welcome, #{name}. You are number #{deli.index(name) + 1} in line."
     
     else 
-    puts "Welcome, #{name}. You are number #{deli.index(name) + 1} in line."
+    puts "Welcome, #{name}. You are number #{deli.index(name)+1} in line."
   end
 end
