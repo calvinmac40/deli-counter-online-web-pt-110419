783706729ddca34ee8317e8476f66b3475ca9c40
Calvin McDowell <calvinmac40@hotmail.com>
2019-11-14 02:35:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+require 'pry'
+
+def line(customer)
+  counter = 1
+  array = []
+  if customer.length == 0
+    puts "The line is currently empty."
+  else customer.length >= 1 
+    customer.each do |number| 
+      array.push("#{counter}. #{number}")
+      counter += 1 
+    end
+    puts "The line is currently: #{array.join(" ")}"
+  end
+end
+
+def take_a_number(deli,name)
+  deli << name
+  if deli.size == 1 
+    puts "Welcome, #{name}. You are number #{deli.index(name) + 1} in line."
+    
+    else 
+    puts "Welcome, #{name}. you are number #{deli.index(name) + 1} in line."
+  end
+end
